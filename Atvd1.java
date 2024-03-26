b9afd7e75f1e1e0b8682626a976df243a33f325a
lucasxavila <164033498+lucasxavila@users.noreply.github.com>
2024-03-26 09:25:02 -0300

    Update Atvd1.java

--- Atvd1.java
+++ Atvd1.java
@@ -4,21 +4,16 @@ public class Atvd1
   public static void main(String[] args) 
   {
     Scanner scanner = new Scanner(System.in);
-    float nota = -1;
+    int nota;
 
-    while (nota < 0 || nota > 10)
-    {
-      System.out.print("Informe sua nota (entre 0 e 10): ");
-      if (scanner.hasNextFloat());
-      {
-        nota = scanner.nextFloat();
-        if (nota < 0 || nota > 10)
-        {
-          System.out.println("Nota inválida. Informe um número entre 0 e 10.");
-        }
+    do {
+      nota = teclado.nextInt();
+      if (nota < 0 || nota > 10) {
+        System.out.println("Nota inválida");
       }
-    }
-    System.out.println("Nota informada: " + nota);
+    } while (nota < 0 || nota > 10)
+      
+    System.out.println("Nota válida: " + nota);
     scanner.close();
   }
 }
